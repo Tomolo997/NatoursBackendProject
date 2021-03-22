69a43b97038abd45d727722f37bc5d5461ce0335
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-22 07:22:26 +0100

    added the post adn get request

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const port = 3000;
 //     .json({ message: 'hello from the server side !', app: 'Natours' });
 // });
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -28,6 +30,37 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  ///:id/:y
+  //optional parameters, we add question mark behind them ? => /api/v1/tours/:id/:y?
+  console.log(req.params);
+  res.status(200).json({
+    status: 'success',
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //with post request we can send data from client to server, data is available in the request. wee need to include middleware, middleware is just a function that modifies the request data
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  //cant send two responses
+  // res.send('done');
 });
 
 app.listen(port, () => {
