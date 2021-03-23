7a36cb7aaa6772bc47ef415abbb72b32b4c2708b
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-23 07:23:46 +0100

    added the DB

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+//connect the mongodb
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB collection succesful');
+  });
+
 //start Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
