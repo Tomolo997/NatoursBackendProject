1c21899c6055cc5491b08cb05639967e7f84ec7d
Tomolo997 <tomazovsenjak7@gmail.com>
2021-04-07 20:37:07 +0200

    added the bookings

--- app.js
+++ app.js
@@ -106,6 +106,10 @@ app.use(
 //The order of thw middlware is important, so global middlewares are at the beginning
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  res.set(
+    'Content-Security-Policy',
+    "default-src 'self' https://*.mapbox.com https://*.stripe.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://js.stripe.com/v3/ 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+  );
   next();
 });
 
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const appError = require('../utils/appError');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -9,7 +10,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //creeate checkouzt seesion
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourID
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -30,3 +33,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session,
   });
 });
+
+exports.createBookingCheckout = async (req, res, next) => {
+  //THis is only temporary, beecause it is unsecure
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) {
+    return next();
+  }
+
+  await Booking.create({ tour, user, price });
+  //get back to the "/" but this time with no query, next middleware is again createBookingCheckout, but it has no tour,user and price, so it will call next
+  res.redirect(req.originalUrl.split('?')[0]);
+};
